73578b400c7b0c78ce059e3f9a855845ae30910f
池田俊彦 <ikedatoshihiko@toikeda-OSX.local>
2017-06-05 18:26:48 +0900

    first commit

---
+++ wsgi.py
@@ -0,0 +1,24 @@
+from flask import Flask,Response
+import requests
+
+app = Flask(__name__)
+default_name = 'Joe Bloggs'
+
+@app.route('/')
+def get_identicon():
+    name    = default_name
+    header  = '<html><head><title>Identidock</title></head><body>'
+    body    = '''<form method="POST">
+                Hello <input type="text" name="name" value="{}">
+                <input type="submit" value="submit">
+                </form>
+                <p>You look like a:
+                <img src="/monster/monster.png"
+                '''.format(name)
+    footer  = '</body></html>'
+
+    return header + body + footer
+
+
+if __name__ == '__main__':
+    app.run()
